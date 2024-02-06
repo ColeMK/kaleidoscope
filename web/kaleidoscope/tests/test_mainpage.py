2f88dc2eb6b62b132aeb979dd112b82481ea6226
WillStonebridge <WStonebridge1@gmail.com>
2024-02-06 17:21:54 -0500

    comments on test_cases

--- web/kaleidoscope/tests/test_mainpage.py
+++ web/kaleidoscope/tests/test_mainpage.py
@@ -10,13 +10,18 @@ class test_Mainpage_Rendering(TestCase):
         self.EXPECTED_RESPONSE_TIME = 5
         return super().setUp()
 
-
+    """
+    Ensure that the webpage contents are as expected
+    """
     def test_Contents(self):
         recieved_response = self.client.get("/")
         print(recieved_response.content)
         self.assertEqual(recieved_response.content, self.EXPECTED_RESPONSE)
 
 
+    """
+    Ensure that the webpage returns a response within 5 seconds of a request
+    """
     def test_Timeliness(self):
         start = datetime.now()
         #recieved_response = requests.get(self.MAINPAGE_URL)
