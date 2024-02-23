5f5bda6788fd4885afc67bf031fafd0f60ddd3d3
WillStonebridge <WStonebridge1@gmail.com>
2024-02-23 12:54:34 -0500

    Multiple lines in checker.py were using string based file parsing. I changed them all to use python's os module. Additionally, it appears that the sound works on my videos.

--- ML/checker.py
+++ ML/checker.py
@@ -3,9 +3,9 @@ import sqlite3
 import os
 from pathlib import Path
 def main():
-    print("meow")
-    upload_path = str(os.path.dirname(os.getcwd()))+"web/kaleidoscope/uploads/"
-    dowload_folder = str(os.path.dirname(os.getcwd()))+"web/kaleidoscope/downloads/"
+    kaliedoscope_dir = os.path.split(os.getcwd())[0]
+    upload_path = os.path.join(kaliedoscope_dir, 'web', 'kaleidoscope', 'uploads')
+    dowload_folder = os.path.join(kaliedoscope_dir, 'web', 'kaleidoscope', 'downloads')
     model_path = "style_ukiyoe_pretrained"
     database = "../web/kaleidoscope/db.sqlite3"
 
@@ -26,10 +26,11 @@ def main():
             count += 1
             specific_id = totalfiles
             cur.execute('SELECT * FROM mainpage_mymodel WHERE id = ?', (specific_id,))
+            #TODO: Cole what does this do? If we using an operating system with \ dir system this will not work.
             process_file = str(((cur.fetchone()[1]).split('/',1)[1])[:-4].replace(" ", "_")) #should split by first instance of /
             #process stuff 
-            uploaded_video_path = upload_path + process_file + ".mp4"
-            stylized_video_path = f"{dowload_folder}{process_file}_{model_path}.mp4"
+            uploaded_video_path = os.path.join(upload_path, process_file) + ".mp4"
+            stylized_video_path = f"{os.path.join(dowload_folder, process_file)}_{model_path}.mp4"
             print("stylize video activate")
             stylize_video(uploaded_video_path, stylized_video_path, model_path)
 
