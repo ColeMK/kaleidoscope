4e60a5d32f51bfd5531b19f2fe85c71821756d6f
ColeMK <kingerycole2@gmail.com>
2024-04-25 00:41:01 -0400

    anything else for final

--- web/kaleidoscope/kaleidoscope/settings.py
+++ web/kaleidoscope/kaleidoscope/settings.py
@@ -17,8 +17,8 @@ import os
 # AWS_SECRET_ACCESS_KEY = 'your-secret-key'
 AWS_STORAGE_BUCKET_NAME = 'sd-kaleidoscope'
 AWS_S3_REGION_NAME = 'us-east-1'  # e.g., 'us-east-1'
-# AWS_ACCESS_KEY_ID = str(os.environ.get('AWS_ACCESS_KEY_ID'))
-# AWS_SECRET_ACCESS_KEY = str(os.environ.get('AWS_SECRET_ACCESS_KEY'))
+AWS_ACCESS_KEY_ID = str(os.environ.get('AWS_ACCESS_KEY_ID'))
+AWS_SECRET_ACCESS_KEY = str(os.environ.get('AWS_SECRET_ACCESS_KEY'))
 #
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
